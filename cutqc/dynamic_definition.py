2610458dd08c71fbbf85eedea628070a08a8c823
Ellie Vogel <ellieovogel@gmail.com>
2024-07-25 10:33:35 -0400

    Changing dynamic_definition.py

--- cutqc/dynamic_definition.py
+++ cutqc/dynamic_definition.py
@@ -77,9 +77,6 @@ class DynamicDefinition(object):
                 subcircuit_entry_probs=merged_subcircuit_entry_probs,
                 num_cuts=self.num_cuts,
                 )
-
-            print('reconstruct works')
-        
             
             smart_order = self.graph_contractor.smart_order
             recursion_overhead = self.graph_contractor.overhead
